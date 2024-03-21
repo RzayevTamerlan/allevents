68f6cd19fb18140de8cbf24be889f3941c519b4b
RzayevTamerlan <tamerlan.rzev@gmail.com>
2024-03-21 19:22:34 +0400

    Add scroll behavior to logo click

--- app/lib/common/components/ui/Logo.jsx
+++ app/lib/common/components/ui/Logo.jsx
@@ -1,8 +1,20 @@
+'use client';
+
 import Image from "next/image";
 
 const Logo = () => {
+  const handleLogoClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
   return (
-    <Image width='200' height='111' src='/assets/logo/logo.png' alt='All events'/>
+    <button onClick={handleLogoClick} className='max-w-[200px] max-h-[111px]'>
+      <Image className='w-full h-full object-cover' width='200' height='111' src='/assets/logo/logo.png'
+             alt='All events'/>
+    </button>
   );
 };
 
