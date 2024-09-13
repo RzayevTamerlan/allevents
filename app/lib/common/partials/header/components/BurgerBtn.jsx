3d7b4ab9e89538c1f4a236b624575888f5df4fb5
RzayevTamerlan <tamerlan.rzev@gmail.com>
2024-09-13 16:12:45 +0400

    Add handler for burger

--- app/lib/common/partials/header/components/BurgerBtn.jsx
+++ app/lib/common/partials/header/components/BurgerBtn.jsx
@@ -6,8 +6,13 @@ const BurgerBtn = () => {
   const toggleBurger = useBurger((state) => state.toggle);
   const isOpen = useBurger((state) => state.isOpen);
 
+  const handleClick = () => {
+    console.log('clicked');
+    toggleBurger();
+  }
+
   return (
-    <button aria-label='Toggle Burger' onClick={toggleBurger} className={`w-6 ${isOpen ? 'translate-y-2' : 'translate-y-0'} transition-all duration-75 lg:hidden flex h-4 flex-col justify-between`}>
+    <button aria-label='Toggle Burger' onClick={handleClick} className={`w-6 ${isOpen ? 'translate-y-2' : 'translate-y-0'} transition-all duration-75 lg:hidden flex h-4 flex-col justify-between`}>
       <div className={`${isOpen ? 'rotate-45' : 'rotate-0'} transform  w-full transition-all duration-300 h-[1px] border border-solid border-black`}></div>
       <div className={`${isOpen ? 'invisible opacity-0' : 'visible opacity-100'} w-full transition-all duration-300 h-[1px] border border-solid border-black`}></div>
       <div className={`${isOpen ? 'rotate-[-45deg] translate-y-[-14px]' : 'rotate-0'} w-full transform transition-all duration-300 h-[1px] border border-solid border-black`}></div>
